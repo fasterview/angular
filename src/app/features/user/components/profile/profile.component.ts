82f0b7be1652a86c18b8db2aca37ddbf30933cbc
Mohamed Abdallah <mr.mohamedabdallah98@gmail.com>
2020-05-31 10:35:59 +0200

    Profile component
    Display user info in profile component and the componies he/she working
    on

--- src/app/features/user/components/profile/profile.component.ts
+++ src/app/features/user/components/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/core/services/auth.service';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  user = null;
+  comps = [];
+  loaded: boolean = false;
+
+  constructor(private _auth: AuthService, private _http: HttpClient) { }
 
   ngOnInit(): void {
+    
+    this._auth.user.subscribe(user => { this.user = user });
+    this.user = this._auth.getUser();
+
+    // Fetch user company
+    this.fetchCompany();
   }
+
+
+  /**
+   * Fetch the companies that user works on
+   */
+  fetchCompany(){
+    this._http.get(environment.url("api/organization"))
+        .subscribe((res: any) => {
+          this.comps = res.organizations
+          this.loaded = true;
+        });
+  }
+
 
 }
